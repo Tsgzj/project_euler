56b3ab77aa71c22345bdaf1547e39545350efb83
Tsgzj <swx@tsgzj.me>
2019-01-22 15:40:14 -0800

    Updated license, need to fix README

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject p_euler "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "none"
+            :year 2019
+            :key "MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :repl-options {:init-ns p-euler.core})
